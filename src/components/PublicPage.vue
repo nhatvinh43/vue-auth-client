10623ed1e916d8589f5f5a3f0414760a49754662
Nhat Vinh Phan <vnphan@agreementexpress.com>
2021-02-18 15:18:37 +0700

    Add PublicPage component
    Allows access for any users, regardless of their log in state.

---
+++ src/components/PublicPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>This is a public page</p>
+</template>
+
+<script>
+export default {
+  name: "PublicPage"
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Login from "@components/Login";
 import Register from "@components/Register";
 import PrivatePage from "@components/PrivatePage";
+import PublicPage from "@components/PublicPage";
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/private-page",
     name: "private-page",
     component: PrivatePage
+  },
+  {
+    path: "/public-page",
+    name: "public-page",
+    component: PublicPage
   }
 ];
 
